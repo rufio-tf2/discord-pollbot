cc41cc7f948fcf4ab1de3aa3a5c885b9cf55f404
Rufio <37485991+rufio-tf2@users.noreply.github.com>
2020-09-05 00:08:56 -0400

    Finish util tests

--- util.spec.js
+++ util.spec.js
@@ -40,18 +40,48 @@ describe("isArrayEmpty", () => {
 
 describe("isEmpty", () => {
   test.each([
-    // [null, true],
-    // [[], true],
-    // [{}, true],
-    // [{}, true],
-    // ["", true],
-    // [1234, true],
-    // [true, true],
-    // [[1, 2, 3], false],
+    [null, true],
+    [[], true],
+    [{}, true],
+    [{}, true],
+    ["", true],
+    [1234, true],
+    [true, true],
+    [[1, 2, 3], false],
     [{ a: 1 }, false],
-    // ["words", false],
+    ["words", false],
   ])("%s => %s", (arg, expected) => {
     const actual = isEmpty(arg);
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("isNull", () => {
+  test.each([
+    [null, true],
+    [[1, 2, 3], false],
+    [{}, false],
+    ["words", false],
+    [1234, false],
+    [true, false],
+  ])("%s => %s", (arg, expected) => {
+    const actual = isNull(arg);
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("isObject", () => {
+  test.each([
+    [{}, true],
+    [{ a: 1 }, true],
+    [[1, 2, 3], false],
+    ["words", false],
+    [1234, false],
+    [true, false],
+  ])("%s => %s", (arg, expected) => {
+    const actual = isObject(arg);
 
     expect(actual).toEqual(expected);
   });
@@ -63,6 +93,33 @@ describe("isObjectEmpty", () => {
     [{ a: 1 }, false],
   ])("%s => %s", (arg, expected) => {
     const actual = isObjectEmpty(arg);
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("isUndefined", () => {
+  test.each([
+    [undefined, true],
+    [null, false],
+    [[1, 2, 3], false],
+    ["words", false],
+    [1234, false],
+    [true, false],
+  ])("%s => %s", (arg, expected) => {
+    const actual = isUndefined(arg);
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("stripLeadingTrailingQuotes", () => {
+  test.each([
+    [`"double quotes"`, `double quotes`],
+    [`'single quotes'`, `single quotes`],
+    [`"leaves inner 'quotes'"`, `leaves inner 'quotes'`],
+  ])("%s => %s", (arg, expected) => {
+    const actual = stripLeadingTrailingQuotes(arg);
 
     expect(actual).toEqual(expected);
   });
