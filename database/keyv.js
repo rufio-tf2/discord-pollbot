7102fa9ae1e6dd3d5e287ccd057d1d571965c746
Rufio <37485991+rufio-tf2@users.noreply.github.com>
2020-09-12 10:00:11 -0400

    Clear db on start

--- database/keyv.js
+++ database/keyv.js
@@ -1,21 +1,26 @@
 const Keyv = require("keyv");
 const sqlite3 = require("sqlite3").verbose();
+const fs = require("fs-extra");
 
-const databaseName = "SlapBotDatabase";
-const pathToDatabase = `./database/${databaseName}`;
+const DATABASE_NAME = "SlapBotDatabase";
 
-const database = new sqlite3.Database(pathToDatabase, (error) => {
+const DATABASE_DIR = "./database/db/";
+const PATH_TO_DATABASE = `${DATABASE_DIR}/${DATABASE_NAME}`;
+
+fs.ensureDirSync(DATABASE_DIR);
+
+const database = new sqlite3.Database(PATH_TO_DATABASE, (error) => {
   if (error) {
     return console.error(
-      `Error creating database \`${databaseName}\``,
+      `Error creating database \`${DATABASE_NAME}\``,
       error.message
     );
   }
 
-  console.log(`Successful connection to the database \`${databaseName}\``);
+  console.log(`Successful connection to the database \`${DATABASE_NAME}\``);
 });
 
-const keyv = new Keyv(`sqlite://${pathToDatabase}`);
+const keyv = new Keyv(`sqlite://${PATH_TO_DATABASE}`);
 
 keyv.on("error", (err) => console.error("Keyv connection error:", err));
 
