fd8048f908a8556dd0b7d8b599f6b71226bf108d
Rufio <37485991+rufio-tf2@users.noreply.github.com>
2020-09-16 08:43:50 -0400

    DB cleanup

--- database/keyv.js
+++ database/keyv.js
@@ -3,14 +3,11 @@ const Keyv = require("keyv");
 const DB_TABLE =
   process.env.NODE_ENV !== "production" ? "develop" : "production";
 
-console.log(process.env.DATABASE_URL);
-
-const keyv = new Keyv(process.env.DATABASE_URL, { table: DB_TABLE });
+const keyv =
+  process.env.NODE_ENV !== "production"
+    ? new Keyv()
+    : new Keyv(process.env.DATABASE_URL, { table: DB_TABLE });
 
 keyv.on("error", (err) => console.error("Keyv connection error:", err));
 
 module.exports = keyv;
-// module.exports = {
-//   get: () => {},
-//   set: () => {},
-// };
