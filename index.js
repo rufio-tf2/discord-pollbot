4f3a219ec6ce500a6570aef1edead2d70e657395
Rufio <reed.tf2@gmail.com>
2020-09-17 20:27:03 -0400

    Test new cache strategy

--- index.js
+++ index.js
@@ -6,9 +6,33 @@ const { Client } = require("discord.js");
 const { onChangeReaction, onMessage } = require("./bot");
 const { getNicknameFromReaction } = require("./bot/discordUtils");
 
+const fetchPartialReaction = async (reaction) => {
+  if (reaction.message.partial) {
+    try {
+      await reaction.message.fetch();
+    } catch {
+      console.error("Failed to fetch cached message ", reaction.message.id);
+    }
+  }
+
+  if (reaction.partial) {
+    try {
+      await reaction.fetch();
+    } catch {
+      console.error("Failed to fetch cached reaction.");
+    }
+  }
+};
+
+const getReactionHandler = (action) => async (reaction, user) => {
+  await fetchPartialReaction(reaction);
+  const username = getNicknameFromReaction(reaction, user.id);
+  onChangeReaction(reaction, username, action);
+};
+
 const TOKEN = process.env.DISCORD_TOKEN;
 
-const client = new Client();
+const client = new Client({ partials: ["REACTION"] });
 
 client.once("ready", () => {
   console.log("Bot ready");
@@ -18,14 +42,8 @@ client.on("message", (message) => {
   onMessage(message);
 });
 
-client.on("messageReactionAdd", async (reaction, user) => {
-  const username = getNicknameFromReaction(reaction, user.id);
-  onChangeReaction(reaction, username, "add");
-});
+client.on("messageReactionAdd", getReactionHandler("add"));
 
-client.on("messageReactionRemove", async (reaction, user) => {
-  const username = getNicknameFromReaction(reaction, user.id);
-  onChangeReaction(reaction, username, "remove");
-});
+client.on("messageReactionRemove", getReactionHandler("remove"));
 
 client.login(TOKEN);
